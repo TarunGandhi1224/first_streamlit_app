0ea09472c4160a8e89c6c0d7cf4ee9fabf683ae0
TarunGandhi1224 <142140926+TarunGandhi1224@users.noreply.github.com>
2023-08-14 00:05:19 +0530

    Update streamlit_app.py
    fixed the function error

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # added a text entry box 
 #create a repetable code block
-def get_fruitvice_data(this_fruit_choice)
+def get_fruitvice_data(this_fruit_choice):
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
