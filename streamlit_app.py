3d1bc5dec550e2a371d9afb5be4c3c797fe09005
TarunGandhi1224 <142140926+TarunGandhi1224@users.noreply.github.com>
2023-08-13 23:21:50 +0530

    Update streamlit_app.py
    adding greetings

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #writing the user entry
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
