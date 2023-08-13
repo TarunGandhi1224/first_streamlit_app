71efaa5d440b90b7301d318846d9834bb9eeb888
TarunGandhi1224 <142140926+TarunGandhi1224@users.noreply.github.com>
2023-08-13 23:54:54 +0530

    Update streamlit_app.py
    modified the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,18 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
 #import requests
-
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # added a text entry box 
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
- if not fruit_choice:
-   streamlit.error("Please select a fruit to get information.")
- else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# take the json version and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# show the normalized version in tabular format
-streamlit.dataframe(fruityvice_normalized)
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+         streamlit.error("Please select a fruit to get information.")
+   else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-  streamlit.error()
+    streamlit.error()
 streamlit.stop()
 #import snowflake.connector
 
