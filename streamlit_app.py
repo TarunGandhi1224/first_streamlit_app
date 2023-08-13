c96a1f1c11e5be7eba75738891cf62ef413b839d
TarunGandhi1224 <142140926+TarunGandhi1224@users.noreply.github.com>
2023-08-13 19:26:04 +0530

    Update streamlit_app.py
    added a text user entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+#created a default text entry box
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#writing the user entry
+streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-
-
+# added a text entry box 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
 # take the json version and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show the normalized version in tabular format
